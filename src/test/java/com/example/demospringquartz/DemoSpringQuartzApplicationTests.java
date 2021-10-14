cd0d2449a54281125ef5d4efcd823ebf341db40c
LDAP\nvbao2 <baonv1001@gmail.com>
2021-10-14 14:55:14 +0700

    first commit

---
+++ src/test/java/com/example/demospringquartz/DemoSpringQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
