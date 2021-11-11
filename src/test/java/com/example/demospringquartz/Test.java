eb2e1cf42c7bfb9acee681ef403cffe808dc150f
LDAP\nvbao2 <baonv1001@gmail.com>
2021-11-11 18:41:42 +0700

    add kafka

--- src/test/java/com/example/demospringquartz/Test.java
+++ src/test/java/com/example/demospringquartz/Test.java
@@ -2,6 +2,13 @@ package com.example.demospringquartz;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println(2657860 & (16-1));
+        System.out.println(63281940  & (16-1));
+        int a = 5;
         System.out.println("Hello World");
+        System.out.println(a);
+
+        a = 6;
+        System.out.println(a);
     }
 }
